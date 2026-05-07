038990d4e1a1ac81dced4265a566ddaff2f0c317
Dhruv <dhruvdpk03@gmail.com>
2026-05-07 17:49:42 +0530

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,23 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { RouterProvider } from "@tanstack/react-router";
+// import { router } from "./router";
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <RouterProvider router={router} />
+// );
+
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import { getRouter } from "./router";
+
+const router = getRouter();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
